92bf88acb6218f4c0ddfd09af5bde54a9c386e7c
L16-F516206700196 <l16f51620@gmail.com>
2026-07-26 11:54:27 -0700

    Update helloWorld.3318bd85.js

--- docs/assets/helloWorld.3318bd85.js
+++ docs/assets/helloWorld.3318bd85.js
@@ -637,16 +637,16 @@ const visualSSlot=z=>{
 }
 let altKey=!1;
 var itemI=[
-	"item1",
-	"item2",
-	"item3",
-	"item4",
-	"item5",
-	"item6",
-	"item7",
-	"item8",
-	"item9",
-	"item0",
+	["item0","item-c0"],
+	["item1","item-c1"],
+	["item2","item-c2"],
+	["item3","item-c3"],
+	["item4","item-c4"],
+	["item5","item-c5"],
+	["item6","item-c6"],
+	["item7","item-c7"],
+	["item8","item-c8"],
+	["item9","item-c9"],
 ];
 var playerInventory=[
 	{name:"Dirt",amount:1,attributes:{}},
@@ -791,7 +791,7 @@ noa.on('tick', function (dt) {
 	}*/
 	for(let i=0;i<itemI.length;i++){
 		let j=itemI[i];
-		document.getElementById(j).src=`../hello-world/textures/${playerInventory[i]?.name||"air"}.png`
+		document.getElementById(j[1]).src=`../hello-world/textures/${playerInventory[i]?.name||"air"}.png`
 	}
 	console.log(`selected hotbar slot is ${selectedHotbarSlotI}, scroll is ${scroll}`)
 	resetSlotOutline();
