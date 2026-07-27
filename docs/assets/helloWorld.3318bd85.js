25705883c9688b1c124d003b86b4f1f3cd1ef623
L16-F516206700196 <l16f51620@gmail.com>
2026-07-27 14:59:51 -0700

    Update helloWorld.3318bd85.js

--- docs/assets/helloWorld.3318bd85.js
+++ docs/assets/helloWorld.3318bd85.js
@@ -714,7 +714,8 @@ let lastSAltFire=performance.now();
 var g=e.playerEntity,m=e.entities.getPositionData(g),fm=noa.entities.getPhysicsBody(g),d=m.width,f=m.height,z=e.rendering.getScene(),a=D("player-mesh",{},z);var move = e.entities.getMovement(g);
 move.maxSpeed = 7.2;move.running=!0;move.jumpImpulse=(84/11);move.jumpTime=0;move.airJumps=0;
 fm.friction=0.5;
-move.standingFriction=0.9;move.runningFriction=0.96;
+//move.standingFriction=0.9;
+move.runningFriction=0.96;
 a.scaling.x=d;a.scaling.z=d;a.scaling.y=f;
 a.material=e.rendering.makeStandardMaterial();e.entities.addComponent(g,e.entities.names.mesh,{mesh:a,offset:[0,f/2,0]});
 
