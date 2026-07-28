5b03dad2890cd80bdf4bd05df26165822de1a490
L16-F516206700196 <l16f51620@gmail.com>
2026-07-28 09:57:25 -0700

    Update helloWorld.3318bd85.js

--- docs/assets/helloWorld.3318bd85.js
+++ docs/assets/helloWorld.3318bd85.js
@@ -18,8 +18,8 @@ var permutationTable=[];
 for(let i=0;i<256;i++){permutationTable.push(i)}
 //perlin by FWJ7 / L16_F51620, normalisation for angleGen3 by GPT 5.4 nano (idk trig lol)
 let seedNum = 0;
-let blockScale=16;
-let heightScaleDiv=64;
+let blockScale=8;
+let heightScaleDiv=128;
 let heightScale=8; //heightScaleDiv/heightScale
 let caveScale=32;
 let caveHeightScaleDiv=24;
