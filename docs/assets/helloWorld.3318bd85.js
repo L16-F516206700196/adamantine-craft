ae55544729095849336b5a187aa887a83f45bc93
L16-F516206700196 <l16f51620@gmail.com>
2026-08-02 17:53:16 -0700

    Update helloWorld.3318bd85.js

--- docs/assets/helloWorld.3318bd85.js
+++ docs/assets/helloWorld.3318bd85.js
@@ -1,5 +1,15 @@
 //left off at line 612, maybe revert scale changes. also line 21 make scale smaller
-import{E as Engine}from"./index.2b3d1184.js";import{C as D}from"./babylon.39bd9ef3.js";
+import{E as Engine}from"./index.2b3d1184.js";
+import{
+	C as D,
+	T as k,
+    M as ae,
+    V as Me,
+    a as re,
+    b as H,
+    c as Ce,
+    d as ve
+}from"./babylon.39bd9ef3.js";
 var opts = {
 	debug: true,
 	showFPS: true,
